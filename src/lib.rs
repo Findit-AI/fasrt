c8a3dc0cf3a0aca14e6fe2f210010c1b01b25a2b
uqio <276879906+uqio@users.noreply.github.com>
2026-03-09 12:00:03 +0800

    test: add unit tests for SRT writer (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(docsrs, allow(unused_attributes))]
 #![deny(missing_docs)]
+#![allow(unused_extern_crates)]
 
 #[cfg(all(not(feature = "std"), feature = "alloc"))]
 extern crate alloc as std;
